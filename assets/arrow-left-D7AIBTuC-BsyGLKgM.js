c222a6c746a449eeda30f93be4d9e2d51a18d114
spikeonthewater-ops <spikeonthewater@gmail.com>
2026-03-23 13:56:14 +0000

    Quick Launch

---
+++ assets/arrow-left-D7AIBTuC-BsyGLKgM.js
@@ -0,0 +1,1 @@
+import{m as e}from"./index-DBNADaUW.js";const r=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],m=e("arrow-left",r);export{m as r};
---
+++ assets/arrow-up-right-BYQ3Q1Vw-BcZATeNz.js
@@ -0,0 +1,1 @@
+import{m as t}from"./index-DBNADaUW.js";const a=[["path",{d:"M7 7h10v10",key:"1tivn9"}],["path",{d:"M7 17 17 7",key:"1vkiza"}]],o=t("arrow-up-right",a);export{o as a};
